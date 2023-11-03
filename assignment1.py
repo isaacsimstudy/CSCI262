83f8c7e6acf9f68a77480e374d41547079366c6b
ISaac <isaacsimstudy@gmail.com>
2023-11-03 17:51:22 +0800

    Completed

--- assignment1.py
+++ assignment1.py
@@ -8,12 +8,25 @@ def hashPassword(firstPassword):
             reducedPassword = (hashedPassword) % count
             marked[reducedPassword] = 1
         else:
-            hashedPassword = hashlib.md5(passwords[reducedPassword].encode()).hexdigest()
+            hashedPassword = hashlib.md5(passwords[reducedPassword].strip('\n').encode()).hexdigest()
             decHashedPassword = int(hashedPassword, 16)
             reducedPassword = decHashedPassword % count
             marked[reducedPassword] = 1
             if (i == 4):
-                rainbowTable.append([firstPassword, hashedPassword])
+                endHash = hashlib.md5(passwords[reducedPassword].strip('\n').encode()).hexdigest()
+                rainbowTable.append([firstPassword, endHash])
+              
+def reduceHash(rainbowPassword):
+    for i in range(5):
+        if(i == 0):
+            hashedRainbowPassword = hashlib.md5(rainbowPassword.encode()).hexdigest()
+            hashedRainbowPassword = int(hashedRainbowPassword, 16)
+            reducedPassword = (hashedRainbowPassword) % count
+        else:
+            hashedPassword = hashlib.md5(passwords[reducedPassword].strip('\n').encode()).hexdigest()
+            decHashedPassword = int(hashedPassword, 16)
+            reducedPassword = decHashedPassword % count   
+    return passwords[reducedPassword].strip('\n')
 
 count = 0
 with open('password.txt', 'r') as file:
@@ -40,4 +53,50 @@ with open('Rainbow.txt', 'w') as file:
     file.close()
 
 inputHash = input("Enter hash to search: ")
+found = False
 
+with open('Rainbow.txt', 'r') as file:
+    rainbowTable = file.readlines()
+    for i in range(len(rainbowTable)):
+        rainbowTable[i] = rainbowTable[i].strip('\n')
+    file.close()
+
+#Check if input hash is in rainbow table
+for i in range(len(rainbowTable)):
+    if(rainbowTable[i].split(',')[1].strip(' \']') == inputHash):
+        print("Hash found in rainbow table.")
+        #Reduce hash to get password, since we know the password is the last one before the hash
+        reducedPassword = rainbowTable[i].split(',')[0].strip('[\'')
+        foundPassword = reduceHash(reducedPassword)
+        hashFoundPassword = hashlib.md5(foundPassword.encode()).hexdigest()
+        if (hashFoundPassword == inputHash):
+            print("Password found: " + foundPassword)
+            found = True
+        break
+    else:
+        if(i == len(rainbowTable) - 1):
+            print("Hash not found in rainbow table.")
+        continue
+
+
+if (found == False):
+    for i in range(len(passwords)):
+        if i == 0:
+            decInputHash = int(inputHash, 16)
+            reducedNumber = decInputHash % count
+            reducedPassword = passwords[reducedNumber].strip('\n')
+            hashReducedPassword = hashlib.md5(reducedPassword.encode()).hexdigest()
+        else:
+            decHashReducedPassword = int(hashReducedPassword, 16)
+            reducedNumber = decHashReducedPassword % count
+            reducedPassword = passwords[reducedNumber].strip('\n')
+            hashReducedPassword = hashlib.md5(reducedPassword.encode()).hexdigest()
+            print(reducedPassword)
+        print(i)
+        if(hashReducedPassword == inputHash):
+            print("Password found: " + reducedPassword)
+            break
+        else:
+            if(i == len(passwords) - 1):
+                print("Password not found.")
+            continue
